32497c1d56aff71263d2458e4acbb4ec5d5ff1c5
stamaniorec <stamaniorec@gmail.com>
2015-11-06 16:56:45 +0200

    Reading from stdin

--- 02_SampleSockets/src/org/elsysbg/ip/sockets/ClientSocketExample.java
+++ 02_SampleSockets/src/org/elsysbg/ip/sockets/ClientSocketExample.java
@@ -1,10 +1,19 @@
 package org.elsysbg.ip.sockets;
 
+import java.util.Scanner;
+
 public class ClientSocketExample {
 	public static void main(String[] args) {
 		System.out.println("GET / HTTP/1.1");
 		System.out.println("Host:www.example.com");
 		System.out.println("Connection:close");
 		System.out.println("");
+		
+		final Scanner scanner = new Scanner(System.in);
+		while(scanner.hasNextLine()) {
+			final String line = scanner.nextLine();
+			System.out.println(line);
+		}
+		scanner.close();
 	}
 }
