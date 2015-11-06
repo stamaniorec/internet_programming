473eb08b2d956ed5b89ee15715447ff5ec118093
stamaniorec <stamaniorec@gmail.com>
2015-11-06 17:51:39 +0200

    Sample echo server implementation

--- 02_SampleSockets/src/org/elsysbg/ip/sockets/EchoServer.java
+++ 02_SampleSockets/src/org/elsysbg/ip/sockets/EchoServer.java
@@ -1,14 +1,30 @@
 package org.elsysbg.ip.sockets;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class EchoServer {
-	
 	private final int port;
 	
 	public EchoServer(int port) {
 		this.port = port;
 	}
 	
-	public void startServer() {
+	public void startServer() throws IOException {
+		final ServerSocket serverSocket = new ServerSocket(port);
+		final Socket socket = serverSocket.accept();
+		PrintStream out = new PrintStream(socket.getOutputStream());
+	
+		final Scanner scanner = new Scanner(socket.getInputStream());
+		while(scanner.hasNextLine()) {
+			final String line = scanner.nextLine();
+			out.println(line);
+		}
 		
+		scanner.close();
+		serverSocket.close();
 	}
 }
