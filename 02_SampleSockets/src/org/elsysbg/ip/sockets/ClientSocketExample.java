ccbf608b939417793f41eca7f038f216a74d4ced
stamaniorec <stamaniorec@gmail.com>
2015-11-06 17:29:37 +0200

    Started an echo server

--- 02_SampleSockets/src/org/elsysbg/ip/sockets/ClientSocketExample.java
+++ 02_SampleSockets/src/org/elsysbg/ip/sockets/ClientSocketExample.java
@@ -1,19 +1,27 @@
 package org.elsysbg.ip.sockets;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class ClientSocketExample {
-	public static void main(String[] args) {
-		System.out.println("GET / HTTP/1.1");
-		System.out.println("Host:www.example.com");
-		System.out.println("Connection:close");
-		System.out.println("");
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		final Socket socket = new Socket("www.example.com", 80);
+		final PrintStream out = new PrintStream(socket.getOutputStream());
+		out.println("GET / HTTP/1.1"); // or /index.html
+		out.println("Host:www.example.com");
+		out.println("Connection:close");
+		out.println("");
 		
-		final Scanner scanner = new Scanner(System.in);
+		final Scanner scanner = new Scanner(socket.getInputStream());
 		while(scanner.hasNextLine()) {
 			final String line = scanner.nextLine();
 			System.out.println(line);
 		}
 		scanner.close();
+		out.close();
+		socket.close();
 	}
 }
