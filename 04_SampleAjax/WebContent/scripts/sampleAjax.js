dca1a0fc8b6dbfa23f308ebe0b3d34e1c6277863
stamaniorec <stamaniorec@gmail.com>
2015-12-18 16:53:45 +0200

    Simple GET request for JSON

--- 04_SampleAjax/WebContent/scripts/sampleAjax.js
+++ 04_SampleAjax/WebContent/scripts/sampleAjax.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
 	"use strict";
-	
+	var ENDPOINT = "http://localhost:3000/tasks/";
+	$.ajax(ENDPOINT, {
+		method: "GET",
+		dataType: "json"
+	}).then(function(response) {
+		console.log(response);
+	});
 });
