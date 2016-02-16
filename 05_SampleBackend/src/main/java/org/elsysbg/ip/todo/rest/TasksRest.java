8ab334725147f3f3a9300f91a6fedad0ecf4cecf
stamaniorec <stamaniorec@gmail.com>
2016-02-16 17:43:12 +0200

    REST from today's class.

---
+++ 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/rest/TasksRest.java
@@ -0,0 +1,64 @@
+package org.elsysbg.ip.todo.rest;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.elsysbg.ip.todo.entities.Task;
+import org.elsysbg.ip.todo.services.TasksService;
+
+@Path("/tasks")
+public class TasksRest {
+	private final TasksService tasksService;
+	
+	@Inject
+	public TasksRest(TasksService tasksService) {
+		this.tasksService = tasksService;
+	}
+	
+	@GET
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public List<Task> getTasks() {
+		return tasksService.getTasks();
+	}
+	
+	@GET
+	@Path("/{taskId}")
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public Task getTask(@PathParam("taskId") long taskId) {
+		return tasksService.getTask(taskId);
+	}
+	
+	@POST
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public Task createTask(Task task) {
+		return tasksService.createTask(task);
+	}
+	
+	@DELETE
+	@Path("/{taskId}")
+	public void deleteTask(@PathParam("taskId") long taskId) {
+		tasksService.deleteTask(taskId);
+	}
+	
+	@PUT
+	@Path("/{taskId}")
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public Task updateTask(@PathParam("taskId") long taskId, Task task) {
+		final Task fromDb = tasksService.getTask(taskId);
+		fromDb.setTitle(task.getTitle());
+		fromDb.setDescription(task.getDescription());
+		return tasksService.updateTask(fromDb);
+	}
+}
---
+++ 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/services/TasksService.java
@@ -0,0 +1,53 @@
+package org.elsysbg.ip.todo.services;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.inject.Singleton;
+
+import org.elsysbg.ip.todo.entities.Task;
+
+@Singleton
+public class TasksService {
+	private List<Task> tasks = Collections.synchronizedList(
+			new LinkedList<Task>()
+	);
+	
+	private long lastId = 0;
+	
+	private synchronized long getAndIncrementNextId() {
+		return ++lastId;
+	}
+	
+	public Task createTask(Task task) {
+		task.setId(getAndIncrementNextId());
+		tasks.add(task);
+		return task;
+	}
+	
+	public List<Task> getTasks() {
+		return new ArrayList<Task>(tasks);
+	}
+	
+	public Task getTask(long taskId) {
+		for(Task task : tasks) {
+			if(taskId == task.getId()) {
+				return task;
+			}
+		}
+		throw new IllegalArgumentException("No task with id: " + taskId);
+	}
+	
+	public Task updateTask(Task task) {
+		deleteTask(task.getId());
+		tasks.add(task);
+		return task;
+	}
+	
+	public void deleteTask(long taskId) {
+		final Task oldTask = getTask(taskId);
+		tasks.remove(oldTask);
+	}
+}
